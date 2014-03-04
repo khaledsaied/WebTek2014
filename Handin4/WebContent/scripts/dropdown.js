c8544bc3b70922cf8b996032acefac361736a8c8
Brian Frederiksen <Fessor@gmail.com>
2014-03-04 10:28:47 +0100

    Added basket.html Added javascript dropdown function

---
+++ Handin4/WebContent/scripts/dropdown.js
@@ -0,0 +1,11 @@
+function dropDownMenu(){
+$(" .drospdown ul ").css({display: "none"}); // Opera Fix
+$(" .drospdown li").hover(function(){
+$(this).find('ul:first').css({visibility: "visible",display: "none"}).slideDown(400);
+},function(){
+$(this).find('ul:first').slideUp(400);
+});
+}
+$(document).ready(function(){ 
+dropDownMenu();
+});
