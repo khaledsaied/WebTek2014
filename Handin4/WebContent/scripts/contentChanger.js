b7e572b3ad69e9c8745ec100d5f939625e3567e3
11662 <mulatjavsen@hotmail.com>
2014-03-04 10:51:37 +0100

    Added AJAX examples from slides

--- Handin4/WebContent/scripts/contentChanger.js
+++ Handin4/WebContent/scripts/contentChanger.js
@@ -25,15 +25,15 @@ $(document).ready(function() {
 });
 
 function addItemsToTable(items) {
-    //Get the table body we we can add items to it
+    // Get the table body we we can add items to it
     var tableBody = document.getElementById("itemtablebody");
-    //Remove all contents of the table body (if any exist)
+    // Remove all contents of the table body (if any exist)
     tableBody.innerHTML = "";
 
-    //Loop through the items from the server
+    // Loop through the items from the server
     for (var i = 0; i < items.length; i++) {
         var item = items[i];
-        //Create a new line for this item
+        // Create a new line for this item
         var tr = document.createElement("tr");
 
         var nameCell = document.createElement("td");
@@ -47,3 +47,44 @@ function addItemsToTable(items) {
         tableBody.appendChild(tr);
     }
 }
+
+//Function from slide AJAX example
+function myfunc()
+{
+	var http;
+	
+	if (!XMLHttpRequest)
+		http = new ActiveXObject("Microsoft.XMLHTTP");
+	else
+		http = new XMLHttpRequest();
+	
+	function sendRequest(httpMethod, url, body, responseHandler) 
+	{
+		http.open(httpMethod, url);
+		http.onreadystatechange = function () 
+			{
+				if (http.readyState == 4 && http.status == 200) 
+					{
+						responseHandler(http.responseText);
+					}
+			};
+			http.send(body);
+	}
+}
+
+//Send a GET request to "/query" without an argument
+sendRequest("GET", "/query", null, function(value) {
+//This code is run when GET data is received
+});
+
+//Include a "q" parameter
+var foo = encodeURIComponent("Mathias");
+sendRequest("GET", "/query?q=" + foo, null, function(value) {
+//This code is run when GET data is received
+});
+
+//Send a POST request with a parameter in request body
+//Use this for updating values on the JAX RS service
+sendRequest("POST", "/query", "q=" + foo, function(value) {
+//...
+});
