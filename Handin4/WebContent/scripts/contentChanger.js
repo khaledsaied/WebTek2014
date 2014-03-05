2d7a3c9289954c0eb0b3ee6e3978a8c1e8fd6f15
11662 <mulatjavsen@hotmail.com>
2014-03-05 16:02:07 +0100

    added function for AJAX

--- Handin4/WebContent/scripts/contentChanger.js
+++ Handin4/WebContent/scripts/contentChanger.js
@@ -97,3 +97,56 @@ function addItemsToTable(items) {
         tableBody.appendChild(tr);
     }
 }
+
+function loadShopURLXMLDoc()
+{
+var xmlhttp;
+var txt,x,xx,i;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    txt="<table border='1'><tr><th>Title</th><th>Artist</th></tr>";
+    x=xmlhttp.responseXML.documentElement.getElementsByTagName("customer");
+    for (i=0;i<x.length;i++)
+      {
+      txt=txt + "<tr>";
+      xx=x[i].getElementsByTagName("customerID");
+        {
+        try
+          {
+          txt=txt + "<td>" + xx[0].firstChild.nodeValue + "</td>";
+          }
+        catch (er)
+          {
+          txt=txt + "<td> </td>";
+          }
+        }
+      xx=x[i].getElementsByTagName("customerName");
+        {
+        try
+          {
+          txt=txt + "<td>" + xx[0].firstChild.nodeValue + "</td>";
+          }
+        catch (er)
+          {
+          txt=txt + "<td> </td>";
+          }
+        }
+      txt=txt + "</tr>";
+      }
+    txt=txt + "</table>";
+    document.getElementById('txtCDInfo').innerHTML=txt;
+    }
+  };
+xmlhttp.open("GET","http://services.brics.dk/java4/cloud/listCustomers",true);
+xmlhttp.send();
+}
