5bf453cf9eb4d30eac96d238148d3f7a2498cafe
khaledsaied <kaledsaied@gmail.com>
2014-03-06 22:02:57 +0100

    Updated ShopService with login functionality.

--- Handin4/src/dk/cs/dwebtek/ShopService.java
+++ Handin4/src/dk/cs/dwebtek/ShopService.java
@@ -2,6 +2,7 @@ package dk.cs.dwebtek;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.jdom2.JDOMException;
 import org.json.JSONArray;
@@ -9,16 +10,19 @@ import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 
 @Path("shop")
 public class ShopService {
     /**
      * Out Servlet session. We will need this for the shopping basket
      */
-    @Context HttpSession session;
+    HttpSession session;
     public ShopService(@Context HttpServletRequest servletRequest) {
         session = servletRequest.getSession();
     }
@@ -59,6 +63,65 @@ public class ShopService {
     	
     	return mJSONArray.toString();
     }
-    
-}
+    @GET
+    @Path("loggedIn")
+    public String loggedIn()
+    {	
+    	if(session.getAttribute("userAttr") != null)
+    		
+    		return session.getAttribute("userAttr").toString();
+    	else
+    		return "NOT !!!";
+    }
+    String r; 
+    @POST
+    @Path("login")
+    public Response login(@FormParam("user") String user, @FormParam("password") String password) throws IOException, JDOMException { 
 
+		r = cloud.login(user, password);
+		
+		if (cloud.responseCode == 200)	{
+			System.out.println("LOGIN SUCCESS!!!!!!!!");
+			session.setAttribute("userAttr", user);
+		}
+		else {
+			System.out.println("UNVALID USER?? RESPONSECODE: " + cloud.responseCode);
+		}	
+    		//String username = "perle";
+    		/*if (session.getAttribute(user) != null) {
+    			username = (String) session.getAttribute("username");
+    			}*/
+    	return Response.status(cloud.responseCode)
+    			.entity("Logget ind som: " + r)
+    			.build();
+    			
+    }
+    
+    int counter = 0;
+    @POST
+    @Path("cart")
+    public String addToCart (@FormParam("id") String itemId,@FormParam("stock") int itemStock) {
+    
+    	HashMap<String,Integer> cart =	(HashMap<String,Integer>) session.getAttribute("cart"); // = new HashMap();
+    	
+    	if(cart == null)
+    	{
+    		cart = new HashMap<String, Integer>();
+    	}
+  
+    	if (cart.containsKey(itemId)) {
+    		cart.put(itemId, cart.get(itemId)+1);
+    	}
+    	else {
+    		cart.put(itemId, 1);
+    	}
+//    	
+//    	if(itemStock > 0)
+//    		
+//    	counter++;
+//    	
+    	
+    	session.setAttribute("cartAttr", cart);
+    	return "http://services.brics.dk/java4/cloud/listItems?shopID=530";
+    }
+ }
