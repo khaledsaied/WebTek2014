789ef13a5a9213e192d0a3b9af89ad70397a62e5
11662 <mulatjavsen@hotmail.com>
2014-03-07 13:49:54 +0100

    Added new AddEventListener

--- Handin4/WebContent/scripts/contentChanger.js
+++ Handin4/WebContent/scripts/contentChanger.js
@@ -129,31 +129,34 @@ function addItemsToTable(items) {
     	}());
 }
     
-function addEventListener(myNode, eventType, myHandlerFunc) {
-    if (myNode.addEventListener)
-        myNode.addEventListener(eventType, myHandlerFunc, false);
+    function addEventListener(myNode, eventType, myHandlerFunc) {
+        if (myNode.addEventListener)
+            myNode.addEventListener(eventType, myHandlerFunc, false);
+        else
+            myNode.attachEvent("on" + eventType,
+                function (event) {
+                    myHandlerFunc.call(myNode, event);
+                });
+    }
+
+    var http;
+    if (!XMLHttpRequest)
+        http = new ActiveXObject("Microsoft.XMLHTTP");
     else
-        myNode.attachEvent("on" + eventType,
-            function (event) {
-                myHandlerFunc.call(myNode, event);
-            });
-}
+        http = new XMLHttpRequest();
 
-var http;
-if (navigator.appName == "Microsoft Internet Explorer")
-    http = new ActiveXObject("Microsoft.XMLHTTP");
-else
-    http = new XMLHttpRequest();
-
-function sendRequest(httpMethod, url, body, responseHandler) {
-    http.open(httpMethod, url, true);
-    http.onreadystatechange = function () {
-        if (http.readyState == 4 && http.status == 200) {
-            responseHandler(http.responseText);
+    function sendRequest(httpMethod, url, body, responseHandler) {
+        http.open(httpMethod, url);
+        if (httpMethod == "POST") {
+            http.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
         }
-    };
-    http.send(body);
-}
+        http.onreadystatechange = function () {
+            if (http.readyState == 4 && http.status == 200) {
+                responseHandler(http.responseText);
+            }
+        };
+        http.send(body);
+    }
 
 
 function loadShopURLXMLDoc()
